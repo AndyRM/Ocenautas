2bbf880dcfac3ff5baa06ae4a6086fe22e29cf5e
Alonso <f.vazquez151000@gmail.com>
2022-05-14 19:04:36 -0500

    Getters and stters

--- backend/app/models/pez.js
+++ backend/app/models/pez.js
@@ -8,6 +8,38 @@ class Pez{
         this.descripcion = descripcion;
         this.imagen = imagen;
     }
+
+    get getNombre(){
+        return this.nombre;
+    }
+
+    get getNombreCientifico(){ 
+        return this.nombre_cientifico;
+    }
+
+    get getEstatus(){  
+        return this.estatus;
+    }
+
+    get getUbicacion(){
+        return this.ubicacion;
+    }
+
+    get getDescripcion(){
+        return this.descripcion;
+    }
+
+    get getImagen(){
+        return this.imagen;
+    }
+
+    set setEstatus(newEstatus){
+        this.estatus = newEstatus;
+    }
+
+    set setUbicacion(newUbicacion){
+        this.ubicacion = newUbicacion;
+    }
 }
 
 module.exports = Pez;
