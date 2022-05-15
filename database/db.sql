11514e5c9708e052029120039398d54b07b902f8
Erykzon <jota.once6mail.com>
2022-05-15 10:52:22 -0500

    Agregando DB primaria

---
+++ database/db.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE datetabase_ocean;
+
+use datetabase_ocean;
+
+CREATE TABLE users(
+  id INT(11) NOT NULL ,
+  username VARCHAR(16) NOT NULL,
+  password VARCHAR(60) NOT NULL,
+  fullname VARCHAR(100) NOT NULL,
+  PRIMARY KEY (id)
+) AUTO_INCREMENT=2;
+
+ALTER TABLE users
+    ADD PRIMARY KEY (id);
+
+ALTER TABLE users
+    MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
+
+DESCRIBE users;
